b0533328100e8b4e2c386bd0931f029572c20d25
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-07 20:27:28 +0530

    terraform-aws-ec2

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow ssh"
+  description = "Allows only ssh connections i.e, port number 22"
+  egress { # usually we allow everything in egress
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # allows from any anyone i.e, any IP address
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+resource "aws_instance" "terraform" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
